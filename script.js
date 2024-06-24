dd9a2d9ff5bd3d9bf04c0ff899d67347ef9e5ab8
dcap-pilot <ivan@dcap.my>
2024-06-24 18:10:59 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,64 @@
+document.addEventListener("DOMContentLoaded", function() {
+    openTab(null, "choose-model"); // Display the first tab by default
+});
+
+function openTab(evt, tabName) {
+    var i, tabcontent, tablinks;
+
+    // Hide all tab contents
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+        tabcontent[i].classList.remove("active");
+    }
+
+    // Remove the active class from all tab buttons
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+
+    // Show the current tab and add the active class to its button
+    document.getElementById(tabName).style.display = "block";
+    document.getElementById(tabName).classList.add("active");
+    if (evt) {
+        evt.currentTarget.className += " active";
+    } else {
+        // Make the first tab button active by default
+        tablinks[0].className += " active";
+    }
+}
+
+function moveToNextTab() {
+    var currentTab = document.querySelector(".tabcontent.active");
+    var nextTab = currentTab.nextElementSibling;
+    if (nextTab && nextTab.classList.contains("tabcontent")) {
+        currentTab.style.display = "none";
+        currentTab.classList.remove("active");
+        nextTab.style.display = "block";
+        nextTab.classList.add("active");
+
+        // Update tab button active class
+        var currentTabIndex = Array.from(document.getElementsByClassName("tabcontent")).indexOf(currentTab);
+        var tablinks = document.getElementsByClassName("tablinks");
+        tablinks[currentTabIndex].classList.remove("active");
+        tablinks[currentTabIndex + 1].classList.add("active");
+    }
+}
+
+function moveToPreviousTab() {
+    var currentTab = document.querySelector(".tabcontent.active");
+    var prevTab = currentTab.previousElementSibling;
+    if (prevTab && prevTab.classList.contains("tabcontent")) {
+        currentTab.style.display = "none";
+        currentTab.classList.remove("active");
+        prevTab.style.display = "block";
+        prevTab.classList.add("active");
+
+        // Update tab button active class
+        var currentTabIndex = Array.from(document.getElementsByClassName("tabcontent")).indexOf(currentTab);
+        var tablinks = document.getElementsByClassName("tablinks");
+        tablinks[currentTabIndex].classList.remove("active");
+        tablinks[currentTabIndex - 1].classList.add("active");
+    }
+}
